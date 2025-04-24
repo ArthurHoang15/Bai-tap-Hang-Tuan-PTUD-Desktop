eea7dfd853d10067c7a03352207c74ab6a6ee78f
ArthurHoang15 <hoangthianhquan2005@gmail.com>
2025-04-25 00:42:58 +0700

    Add ListView-TreeView exercise and module 3 exercise

---
+++ 31231021898_Hoàng Thi Anh Quân/Tuan3/Module3BaiTap_HoangThiAnhQuan/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Module3BaiTap_HoangThiAnhQuan
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loadFolderButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // loadFolderButton
+            // 
+            this.loadFolderButton.Location = new System.Drawing.Point(12, 12);
+            this.loadFolderButton.Name = "loadFolderButton";
+            this.loadFolderButton.Size = new System.Drawing.Size(100, 30);
+            this.loadFolderButton.TabIndex = 0;
+            this.loadFolderButton.Text = "Load Folder";
+            this.loadFolderButton.UseVisualStyleBackColor = true;
+            this.loadFolderButton.Click += new System.EventHandler(this.loadFolderButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 48);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(760, 400);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 480);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Label1";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 454);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(760, 120);
+            this.flowLayoutPanel1.TabIndex = 3;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 586);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.loadFolderButton);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loadFolderButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
---
+++ 31231021898_Hoàng Thi Anh Quân/Tuan3/Module3BaiTap_HoangThiAnhQuan/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System.Windows.Forms;
+
+namespace Module3BaiTap_HoangThiAnhQuan
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        // Sự kiện khi nhấn nút "Load Folder"
+        private void loadFolderButton_Click(object sender, EventArgs e)
+        {
+            // Tạo một FolderBrowserDialog để chọn thư mục
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.RootFolder = Environment.SpecialFolder.MyComputer;
+
+            // Hiển thị dialog và kiểm tra kết quả
+            var res = dialog.ShowDialog();
+            if (res == DialogResult.OK)
+            {
+                // Lấy đường dẫn thư mục được chọn
+                string path = dialog.SelectedPath;
+                string[] files = Directory.GetFiles(path);
+
+                // Xóa các control cũ trong FlowLayoutPanel (nếu có)
+                flowLayoutPanel1.Controls.Clear();
+
+                // Duyệt qua từng file trong thư mục
+                foreach (var file in files)
+                {
+                    // Kiểm tra định dạng file (chỉ lấy .jpg và .png)
+                    if (file.EndsWith(".jpg") || file.EndsWith(".png"))
+                    {
+                        // Tạo một PictureBox mới để hiển thị hình ảnh thu nhỏ
+                        PictureBox pictureBox = new PictureBox();
+                        pictureBox.Image = new System.Drawing.Bitmap(file); // Tải hình ảnh
+                        pictureBox.SizeMode = PictureBoxSizeMode.StretchImage; // Căn chỉnh hình ảnh
+                        pictureBox.Tag = file; // Lưu đường dẫn file vào Tag
+                        pictureBox.Size = new System.Drawing.Size(100, 100); // Kích thước thumbnail
+
+                        // Gắn sự kiện Click cho PictureBox thu nhỏ
+                        pictureBox.Click += PictureBox_Click;
+
+                        // Thêm PictureBox vào FlowLayoutPanel
+                        flowLayoutPanel1.Controls.Add(pictureBox);
+                    }
+                }
+            }
+        }
+
+        // Sự kiện khi nhấn vào một PictureBox thu nhỏ
+        private void PictureBox_Click(object sender, EventArgs e)
+        {
+            if (sender is PictureBox pictureBox)
+            {
+                // Lấy đường dẫn hình ảnh từ Tag
+                string imgPath = pictureBox.Tag.ToString();
+
+                // Hiển thị hình ảnh trong PictureBox chính
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                pictureBox1.Image = new System.Drawing.Bitmap(imgPath);
+
+                // Cập nhật thông tin trên Label
+                label1.Text = $"File {imgPath} is loaded.";
+            }
+        }
+    }
+}
